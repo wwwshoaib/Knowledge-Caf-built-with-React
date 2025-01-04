6e9d953f544a0af30058eda79cc82aa4d25ca0ad
Shoaib <shoaibchemistrysust@gmail.com>
2025-01-04 21:57:21 +0600

    project setup and tailwind setup done

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className='bg-blue-800 text-center text-3xl'>Knowlege Cafe</h1>
+     
+    </>
+  )
+}
+
+export default App
