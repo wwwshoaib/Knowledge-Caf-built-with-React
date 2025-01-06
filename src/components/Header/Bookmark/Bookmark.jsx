3b7528af7dbdbe5750bbf901baa8662408535b71
Shoaib <shoaibchemistrysust@gmail.com>
2025-01-06 14:49:58 +0600

    Bookmar button mae

---
+++ src/components/Header/Bookmark/Bookmark.jsx
@@ -0,0 +1,20 @@
+
+
+import PropTypes from "prop-types";
+const Bookmark = ({bookmark}) => {
+
+    const {title} = bookmark;
+  
+    return (
+        <div className=" space-y-2 my-2">
+            <h2 className="p-4 bg-violet-200 text-xl font-semibold rounded-lg">{title}</h2>
+        
+        </div>
+    );
+};
+Bookmark.propTypes = {
+       bookmarks: PropTypes.array, 
+       bookmark: PropTypes.object
+   } 
+  
+export default Bookmark;
--- src/components/Header/Bookmarks/Bookmarks.jsx
+++ src/components/Header/Bookmarks/Bookmarks.jsx
@@ -1,10 +1,14 @@
 
 import PropTypes from "prop-types";
+import Bookmark from "../Bookmark/Bookmark";
 const Bookmarks = ({bookmarks}) => {
     return (
-        <div className="md:w-1/3 border-2  my-4 p-2">
-            <h2>Bookmarks: {bookmarks.length}</h2>
-            
+        <div className="md:w-1/3 border-2  my-4 p-2 bg-red-50">
+            <h2 className="text-center text-2xl font-semibold p-3 border-1">Bookmarks: {bookmarks.length}</h2>
+            {/* bookmarks */} 
+            {
+                bookmarks.map((bookmark) => <Bookmark key ={bookmark.id} bookmark = {bookmark}></Bookmark>)
+            }
         </div>
     );
 };
