04fe3dc79a6f768f93ab7c48961f8c8a2d87d34f
Shoaib <shoaibchemistrysust@gmail.com>
2025-01-06 19:38:17 +0600

    blogs length removed

--- src/components/Header/Blogs/Blogs.jsx
+++ src/components/Header/Blogs/Blogs.jsx
@@ -2,38 +2,32 @@ import { useEffect, useState } from "react";
 import Blog from "../../Blog/Blog";
 import PropTypes from 'prop-types';
 
-const Blogs = ({handleAddBookmark, handleTotalBookReadingTime}) => {
-    const [blogs, setBlogs ,  ] = useState([]);
+const Blogs = ({ handleAddBookmark, handleTotalBookReadingTime }) => {
+    const [blogs, setBlogs,] = useState([]);
 
-    useEffect ( () => {
+    useEffect(() => {
         fetch('blogs.json')
-        .then(res => res.json())
-        .then(data => setBlogs(data))
-    } ,[])
+            .then(res => res.json())
+            .then(data => setBlogs(data))
+    }, [])
     return (
         <div className="md:w-2/3 border-2 my-4 p-2">
-           
-            <h1>Blogs: {blogs.length}</h1> 
             {
-                blogs.map(blog => <Blog 
-                    key = {blog.id} 
+                blogs.map(blog => <Blog
+                    key={blog.id}
 
-                    blog = {blog}
-                    handleAddBookmark = {handleAddBookmark}
-                    handleTotalBookReadingTime = {handleTotalBookReadingTime}
-                    >
-                     
-                    </Blog>)
+                    blog={blog}
+                    handleAddBookmark={handleAddBookmark}
+                    handleTotalBookReadingTime={handleTotalBookReadingTime}
+                >
+                </Blog>)
             }
-            
         </div>
     );
 };
 Blogs.propTypes = {
-       handleAddBookmark: PropTypes.func,
-       handleTotalBookReadingTime: PropTypes.number
-   }
-   
-
+    handleAddBookmark: PropTypes.func,
+    handleTotalBookReadingTime: PropTypes.number
+}
 
 export default Blogs;
